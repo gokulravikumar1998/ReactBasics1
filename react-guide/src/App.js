e3d8d79a180cf35608333c1adc8b943f540325b0
gokulravikumar1998 <gokulit1620@gmail.com>
2022-07-04 10:26:17 +0530

    manipulationg the state and children

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -6,27 +6,32 @@ class App extends Component {
 
   state = {
     persons:[
-      {
-        name: "Gokul",
-        age: 23
-      },
-      {
-        name:"Balu",
-        age: 19
-      },
-      {
-        name:"Raj",
-        age: 31
-      }
-    ]
+      {name: "Gokul",age: 23},
+      {name:"Balu",age: 19},
+      {name:"Raj",age: 31}
+    ],
+    otheState:'Other val'
   }
 
+  switchNameHandler = () =>{
+    //this can only be used in the funciton syntax here ,
+    // not es5 syntax so that we can reach out to the state
+
+    //this.state.persons[0].name = 'Max' ***NOT RIGHT WAY**
+    this.setState({
+      persons:[
+        {name: "Max",age: 23},
+        {name:"Balu",age: 19},
+        {name:"Raj",age: 31}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi,I'm React Developer</h1>
         <p>This is working</p>
-        <button>Switch Button</button>
+        <button onClick={this.switchNameHandler}>Switch Button</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Here from Balu</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/> 
