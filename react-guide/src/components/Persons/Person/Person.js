d840d75001cfe10f647f854574f132e4a18a1b55
gokulravikumar1998 <gokulit1620@gmail.com>
2022-07-23 17:24:30 +0530

    alternative of hoc aux {Fragment}

--- react-guide/src/components/Persons/Person/Person.js
+++ react-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React , {Component} from "react"
+import React , {Component,Fragment} from "react"
 import './Person.css'
 import Radium from "radium"
 import styled from "styled-components"
@@ -21,12 +21,12 @@ class Person extends Component {
 render(){
    console.log('[Person.js] rendering ....') 
     return (
+        <Fragment>
     
-    <StyleDiv>
         <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} year old</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-    </StyleDiv>
+        </Fragment>
     )
 }
 }
